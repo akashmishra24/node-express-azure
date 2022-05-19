22265ee834fc128e8cbabed2b86c111d7e49521d
akashmishra24 <89220556+akashmishra24@users.noreply.github.com>
2022-05-19 20:51:44 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,20 @@
 import express from 'express';
 import { engine } from 'express-handlebars';
 
-const app = express();
-
-app.engine('.hbs', engine({extname: '.hbs'}));
-app.set('view engine', '.hbs');
-app.set('views', './views');
-
 const config = require('./config');
 const express = require('express');
 const path = require('path');
-// const exphbs = require('express-handlebars');
-//const { engine } = require('express-handlebars');
+const exphbs = require('express-handlebars');
 
 const index = require('./routes/index');
 const who = require('./routes/who');
 const contact = require('./routes/contact');
 
-// app.set('views', path.join(__dirname, 'views'));
-// app.engine('.hbs', exphbs.engine({defaultLayout: 'main'}));
+const app = express();
 
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', exphbs.engine({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 app.set('port', config.port);
 
 app.use('/', express.static('public'))
